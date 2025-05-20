eca9f3de40ad2d15e2df58d10df16c898792c0bd
Saiguer10 <eljosemsp@gmail.com>
2025-05-20 13:21:41 +0200

    "Commit con prueba hecha en postman de usuario"

--- gateway/src/main/java/com/ironhack/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/ironhack/gateway/config/GatewayConfig.java
@@ -7,8 +7,18 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
-// ups! Alguien ha hackeado esto y ahora no tenemos la config
 
+// ups! Alguien ha hackeado esto y ahora no tenemos la config
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user", r -> r.path("/api/user/**")
+                    .uri("lb://users-micro"))
+                .route("account", r -> r.path("/api/account/**")
+                    .uri("lb://accounts-micro")
+                ).build();
+
+    }
 }
 
 // ¿Qué es lb???
